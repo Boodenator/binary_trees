886af63bd6a22bec7e0ad2f0673026a7dbc4b4a0
Boodenator <boode1000@gmail.com>
2024-06-07 04:55:02 -0700

    task1 fx

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@
  *         Otherwise - new ptr to the new node.
  *
  * Description: If parent present with left-aux, a new node
- *              subistitute it and the old left-aux is set to
+ *              replace it and the old left-aux is set to
  *              the new node left-aux.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
